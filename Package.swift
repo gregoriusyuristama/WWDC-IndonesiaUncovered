5f3c7c07e065f6628751bf2875210a46a227be17
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2023-12-05 01:15:51 +0700

    fix: lock orientation to landscape

--- Package.swift
+++ Package.swift
@@ -20,17 +20,15 @@ let package = Package(
             teamIdentifier: "U43A7X58YW",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .location),
-            accentColor: .presetColor(.orange),
+            appIcon: .asset("AppIcon"),
+            accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [
                 .pad,
                 .phone
             ],
             supportedInterfaceOrientations: [
-                .portrait,
                 .landscapeRight,
-                .landscapeLeft,
-                .portraitUpsideDown(.when(deviceFamilies: [.pad]))
+                .landscapeLeft
             ]
         )
     ],
